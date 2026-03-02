916f7d0baa61c102c6c86da91d11d851f87735e3
SamayaLyons <139393648+Samgirl19@users.noreply.github.com>
2026-03-02 03:02:33 +0000

    Pending changes exported from your codespace

--- apis.py
+++ apis.py
@@ -7,11 +7,11 @@ load_dotenv(find_dotenv())
 NASA_API_KEY = os.getenv("NASA_API_KEY", "DEMO_KEY")
 URL = "https://api.nasa.gov/planetary/apod"
 
-def apod_generator(url, api_key):
-    params = {"api_key": api_key}
+def apod_generator():
+    params = {"api_key": NASA_API_KEY}
 
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(URL, params=params)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
